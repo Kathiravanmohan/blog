702ab9f83b4ef2dd4e07a3e77f688976b3915f14
Kathiravanmohan <kathiravanmohan19@gmail.com>
2023-09-05 19:53:13 +0530

    mock api added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-export const API_URL="https://648f0af775a96b6644449e25.mockapi.io/blog"
+export const API_URL="https://64f737b19d775408495352e5.mockapi.io/Blog"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
